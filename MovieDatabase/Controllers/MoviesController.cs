c8190060128d001d92cebaed333285dc42f5a102
ethanwcarn <carn0@byu.edu>
2026-02-17 12:09:37 -0700

    Revert "Mission07 carn"

--- MovieDatabase/Controllers/MoviesController.cs
+++ MovieDatabase/Controllers/MoviesController.cs
@@ -13,16 +13,6 @@ namespace MovieDatabase.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
-        {
-            var movies = _context.Movies
-                .OrderBy(m => m.Title)
-                .ToList();
-
-            return View(movies);
-        }
-
-        [HttpGet]
         public IActionResult Add()
         {
             return View(new Movie());
@@ -42,68 +32,6 @@ namespace MovieDatabase.Controllers
 
             return View("Confirmation", movie);
         }
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var movie = _context.Movies.Find(id);
-
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return View(movie);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Movie movie)
-        {
-            if (id != movie.Id)
-            {
-                return BadRequest();
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return View(movie);
-            }
-
-            _context.Update(movie);
-            _context.SaveChanges();
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet]
-        public IActionResult Delete(int id)
-        {
-            var movie = _context.Movies.Find(id);
-
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return View(movie);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var movie = _context.Movies.Find(id);
-
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            _context.Movies.Remove(movie);
-            _context.SaveChanges();
-
-            return RedirectToAction(nameof(Index));
-        }
     }
 }
+
